9a4947169a2f9bfafa1a8b0d55653eaa585042e2
naitoDoriso <89417602+naitoDoriso@users.noreply.github.com>
2024-01-08 23:43:09 -0300

    init

---
+++ admin.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>adm</title>
+</head>
+<body>
+    <?php
+        session_start();
+        $senha = "3141592653589CastorChines3141592653589";
+        if (!empty($_SESSION["adm"])) {
+            if (!empty($_POST)) {
+                $f = fopen("./palavra.json", "w");
+                fwrite($f, json_encode(["word"=>$_POST["palavra"]], JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE));
+                header("Location: ./index.php");
+            }
+
+            if ($_SESSION["adm"] == $senha) {?>
+
+    <h1>Logado como adm</h1>
+    <div style="display: inline-flex;">
+        palavra do dia:
+        <form method="post">
+            <input type="text" name="palavra">
+            <input type="submit" value="trocar">
+        </form>
+    </div>
+
+                <?php return 0;
+            }
+        }
+
+        if (!empty($_POST)) {
+            if ($_POST["pass"] == $senha) {
+                $_SESSION["adm"] = $senha;
+                header("Refresh:0");
+            } else {
+                echo "<span style='color:red'>senha errada paizão!</span>";
+            }
+        }
+    ?>
+    <div style="display: inline-flex;">
+        senha:
+        <form method="post">
+            <input type="text" name="pass">
+            <input type="submit" value="sign in">
+        </form>
+    </div>
+</body>
+</html>
+
+<?php
+// Senha: 3141592653589CastorChines3141592653589
+?>
---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="https://icons.veryicon.com/png/o/miscellaneous/building-materials-website/speaking-speak.png">
+    <title>Palavra do dia</title>
+    <style>
+        body {
+            background-color: #68478d;
+        }
+
+        #palavra {
+            background-color: wheat;
+            outline: 0;
+            padding: 110px 100px;
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 50px;
+            border: 0;
+            color: black;
+            width: fit-content;
+            height: fit-content;
+            border-radius: 8px;
+            transition: padding .1s linear;
+            top: 50%;
+            left: 50%;
+            position: absolute;
+            transform: translate(-50%,-50%);
+            user-select: none;
+            box-shadow: 10px 10px 0 5   px black;
+        }
+
+        #palavra:hover {
+            padding: 111px 101px;
+        }
+
+
+        #request {
+            background-color: #9f9;
+            outline: 0;
+            padding: 6px 12px;
+            font-size: 24px;
+            border: 0;
+            color: #666;
+            width: 130px;
+            height: 40px;
+            border-radius: 8px;
+            transition: background-color .1s linear, border-radius .1s linear, width .1s linear, height .1s linear, font-size .1s linear;
+            cursor: pointer;
+            top: 50%;
+            left: 50%;
+            position: absolute;
+            transform: translate(-50%,-50%)
+        }
+
+        #request:hover {
+            background-color: #4f4;
+            border-radius: 0px;
+            width: 160px;
+            height: 80px;
+            font-size: 30px;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        session_start();
+        if (!empty($_POST) || !empty($_SESSION["clicked"])) {
+            $f = fopen("./palavra.json", "r");
+            $json = json_decode(fread($f, filesize("./palavra.json")));
+            if (empty($_SESSION["clicked"])) header("Refresh:0");
+            $_SESSION["clicked"] = "1";
+        ?>
+
+        <center><h2 style="color: white;">Volte amanhã às 18:40 para uma nova palavra.</h2></center>
+        <h1 id="palavra"><?= $json->word ?></h1>
+
+        <?php }
+
+        else {?>
+
+    <form method="post">
+        <button id="request" name="request">palavra</button>
+    </form>
+
+    <?php } ?>
+</body>
+</html>
